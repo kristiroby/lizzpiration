500e9974ecb7aa24f79bcd10ed556adf6eac7bd8
kristiroby@gmail.com <kristiroby@gmail.com>
2019-09-12 17:48:12 -0500

    created api

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Lizzo
-        </p>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor (props){
+    super(props)
+    this.state = {
+      lizzoQuotes: lizzoQuotes,
+      quote: '',
+      source: ''
+      // }
+    }
+  }
+
+  // fetch(https://us-central1-corded-essence-252619.cloudfunctions.net/function-1)
+
+  render () {
+    return (
+      <div>
+        {this.state.lizzoQuotes[0].quote}
+      </div>
+    )
+  }
 }
 
 export default App;
