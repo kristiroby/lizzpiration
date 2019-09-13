98c0f5c5b721077c59152afe8c59f1347a5a0d72
kristiroby@gmail.com <kristiroby@gmail.com>
2019-09-13 14:58:35 -0500

    button now changes quote

--- src/App.js
+++ src/App.js
@@ -9,20 +9,36 @@ class App extends React.Component {
     }
   }
 
-  componentDidMount() {
-    fetch('https://us-central1-corded-essence-252619.cloudfunctions.net/function-1')
+fetchQuote = () => {
+  fetch('https://us-central1-corded-essence-252619.cloudfunctions.net/function-1')
       .then(response => {
         return response.json()
       }).then(data => {
         this.setState({quotes: data})
       })
   }
-
+  componentDidMount() {
+    this.fetchQuote()
+  } 
+  
   render () {
     return (
       <div>
-        {this.state.quotes.quote} <br />
-        {this.state.quotes.source}
+        <h1>Lizzpiration</h1>
+      <div className="quote-source">
+        <div className="quotelines">
+          {this.state.quotes.quote}
+        </div>
+        <div className="quotelines">
+          {this.state.quotes.source} 
+        </div>
+        <div>
+          <button onClick={this.fetchQuote} >Get the Juice</button>
+        </div>
+        <div>
+          <img src="http://placekitten.com/g/200/300" alt="kitty cat kitty cat purrrr" height="300px" width="auto"></img>
+        </div>
+      </div>
       </div>
     )
   }
