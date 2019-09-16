6cacb493a6b6cea6a5120dd5ac79481f9b5905ea
kristiroby@gmail.com <kristiroby@gmail.com>
2019-09-16 16:43:33 -0500

    testing

--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,6 @@ import Aside from './Aside.js';
 import Quote from './Quote.js';
 
 class App extends React.Component {
-  constructor (props){
-    super(props)
-    this.state = {
-      quotes: {}
-    }
-  }
-
-  
 
   render () {
     return (
